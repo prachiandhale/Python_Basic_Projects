36c5c76d178688e505c576d904ee67013e126ff2
prachiandhale <prachiandhale1212@gmail.com>
2026-06-28 19:25:31 +0530

    basic scientific calculator function

--- task12.9.py
+++ task12.9.py
@@ -52,4 +52,46 @@ def basic_arithmetic():
 
     except Exception as e:
         print("Error:", e )
+   
+        
+# Function for scientific calculations
+def scientific_calculator():
+    try:
+        print("\n1. Square Root")
+        print("2. Power")
+        print("3. Factorial")
+
+        choice = int(input("Enter Choice: "))
+
+        if choice == 1:
+            num = float(input("Enter Number: "))
+            result = math.sqrt(num)
+            operation = f"sqrt({num})"
+
+        elif choice == 2:
+            base = float(input("Enter Base: "))
+            power = float(input("Enter Power: "))
+            result = math.pow(base, power)
+            operation = f"{base}^{power}"
+
+        elif choice == 3:
+            num = int(input("Enter Integer: "))
+            result = math.factorial(num)
+            operation = f"{num}!"
+
+        else:
+            print("Invalid Choice!")
+            return
+
+        print("Result:", result)
+
+        time = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+        history[time] = f"{operation} = {result}"
+
+    except ValueError:
+        print("Invalid Input!")
+
+    except Exception as e:
+        print("Error:", e)
+
         
