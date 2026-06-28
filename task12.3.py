af11d24f21f330fee522bcd28568521442513e93
prachiandhale <prachiandhale1212@gmail.com>
2026-06-28 16:28:47 +0530

    these will add marks

--- task12.3.py
+++ task12.3.py
@@ -5,3 +5,17 @@ class Student:
         self.name = name
         self.roll_no = roll_no
         self.marks_list = []
+        
+     # Method to add marks
+    def add_mark(self, mark):
+        try:
+            mark = float(mark)
+
+            # Check if marks are within valid range
+            if mark < 0 or mark > 100:
+                raise ValueError("Marks should be between 0 and 100.")
+
+            self.marks_list.append(mark)
+
+        except ValueError as e:
+            print("Invalid Input!", e)
