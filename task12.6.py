82d58caec07eb019e13df0d1a026341b3049ad0b
prachiandhale <prachiandhale1212@gmail.com>
2026-06-28 17:52:25 +0530

    these will store squares in list

--- task12.6.py
+++ task12.6.py
@@ -4,5 +4,9 @@ square = lambda x: x ** 2
 try:
     # Generate numbers from 1 to 20
     numbers = range(1, 21)
+    
+     # Store squares in a list using lambda function
+    square_list = list(map(square, numbers))
+    
 except:
     pass
