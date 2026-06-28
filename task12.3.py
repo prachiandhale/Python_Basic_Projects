06e222289f863ea1a07904787b070301421c89c1
prachiandhale <prachiandhale1212@gmail.com>
2026-06-28 16:18:55 +0530

    these will take student details

---
+++ task12.3.py
@@ -0,0 +1,7 @@
+class Student:
+
+    # Constructor to initialize student details
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+        self.marks_list = []
