50be7b8291b29edcf8b2c8714fba5a643c975830
prachiandhale <prachiandhale1212@gmail.com>
2026-06-28 16:51:39 +0530

    these will search student

--- task12.4.py
+++ task12.4.py
@@ -31,6 +31,21 @@ def student_database():
                 })
 
                 print("Student record added successfully!")
+                
+            elif choice == 2:
+                roll_no = input("Enter Roll Number to Search: ")
+
+                # Search using get()
+                record = students.get(roll_no)
+
+                if record:
+                    print("\nStudent Found")
+                    print("Roll No :", roll_no)
+                    print("Name :", record["Name"])
+                    print("Age :", record["Age"])
+                    print("City :", record["City"])
+                else:
+                    print("Student not found!")
 
         
         except:
