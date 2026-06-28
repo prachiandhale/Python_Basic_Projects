76cf4b4b8d31726753ccc813eb4cdcfd5dd2ed7d
prachiandhale <prachiandhale1212@gmail.com>
2026-06-28 12:37:29 +0530

    these will print length of string

---
+++ task12.1.py
@@ -0,0 +1,9 @@
+# Function to analyze a string
+def analyze_string(s):
+    
+    # Handle empty string case
+    if len(s) == 0:
+        print("Empty string entered!")
+        return
+    # Print length of the string
+    print("Length of the string :", len(s))
